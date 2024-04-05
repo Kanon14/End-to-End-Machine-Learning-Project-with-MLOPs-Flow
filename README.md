9f36edffbb2aa81bc49811b4c839125e196b60ff
Kanon14 <geochua144@gmail.com>
2024-04-05 17:42:44 +0800

    model evaluation completed

--- README.md
+++ README.md
@@ -10,3 +10,43 @@
 7. Update the pipeline
 8. Update the main.py
 9. Update the dvc.yaml
+
+
+# How to run?
+
+### STEPS:
+
+```bash
+https://github.com/Kanon14/End-to-End-Machine-Learning-Project-with-MLOPs-Flow
+```
+
+### STEP 01: Create a conda environment after opening the repository
+```bash
+conda create -n mlproj python=3.8 -y
+```
+
+```bash
+conda activate mlproj
+```
+
+
+### STEP 02: Install the requirements
+```bash
+pip install -r requirements.txt
+```
+
+### STEP 03: Run the pipeline
+```bash
+python app.py
+```
+
+Now,
+```bash
+open up your local host and port
+```
+
+
+
+## ML Flow
+[Documentation](https://mlflow.org/docs/latest/index.html)
+
--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from mlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipe
 from mlProject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from mlProject.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 from mlProject.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
+from mlProject.pipeline.stage_05_model_evaluation import ModelEvaluationTrainingPipeline
+
 
 STAGE_NAME = "Data Ingestion Stage"
 try:
@@ -42,6 +44,17 @@ try:
     logger.info(f">>>>>> Stage {STAGE_NAME} started <<<<<<")
     model_trainer = ModelTrainerTrainingPipeline()
     model_trainer.main()
+    logger.info(f">>>>>> Stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Model Evaluation Stage"
+try:
+    logger.info(f">>>>>> Stage {STAGE_NAME} started <<<<<<")
+    model_evaluation = ModelEvaluationTrainingPipeline()
+    model_evaluation.main()
     logger.info(f">>>>>> Stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
     logger.exception(e)
